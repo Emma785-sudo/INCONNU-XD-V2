3388600579a21eb1a22c02948d5bb40cbe9b19a1
Emma785-sudo <emmanuelkayumbayi@gmail.com>
2025-08-25 21:10:46 +0200

    Update config.cjs
    Signed-off-by: Emma785-sudo <emmanuelkayumbayi@gmail.com>

--- config.cjs
+++ config.cjs
@@ -3,9 +3,9 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~OxRlgbyb#CyTaTejlQk170ktC8t1xYabjbriz2n0H-sPMUB1SqTs"
+  SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~b5hxASYB#MLd88lqITfMWYjcqwWJXFzLX8iX-K_WEMb3I-Al3ul0"
   PREFIX: process.env.PREFIX || '.',
-   BOT_NAME: process.env.BOT_NAME || "𝙳𝚈𝙻𝙰𝙽",
+   BOT_NAME: process.env.BOT_NAME || "💌~𓆩𝕄𝐫 𝖑𝖎𝖌𝖍𝖙𒆜—͟͞͞𝗚𝔯𝔦𝔪𝔪𓆪~👻",
    BOT: process.env.BOT || "hello 👋",
   NEW_CMD: process.env.NEW_CMD || "ᴀᴅᴅᴠᴀʀ\n│ sᴜᴅᴏ\n| inconnu",
   CAPTION: process.env.CAPTION|| "ᴘᴏᴡᴇʀᴇᴅ ʙʏ inconnu boy",
@@ -40,8 +40,8 @@ const config = {
   MODE: process.env.MODE || "public",
   DELETED_MESSAGES_CHAT_ID: process.env.DELETED_MESSAGES_CHAT_ID || "554488138425@s.whatsapp.net",
   OWNER_NAME: process.env.OWNER_NAME || "INCONNU BOY",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "243983278829",
-  SUDO_NUMBER: process.env.SUDO_NUMBER || "243983278829",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "224628306483",
+  SUDO_NUMBER: process.env.SUDO_NUMBER || "224628306483",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
